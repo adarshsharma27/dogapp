aa756c525d4fe4dfd6c52abc965378ca6ffee888
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2022-12-17 12:38:19 +0530

    Read Me added and  Formatting changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import SingleDog from "./components/SingleDog";
-import TopHeader from "./components/TopHeader"
+import TopHeader from "./components/TopHeader";
 
 function App() {
   return (
     <>
-    <TopHeader/>
-    <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/dog/:name" element={<SingleDog/>}/>
-    </Routes>
+      <TopHeader />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dog/:name" element={<SingleDog />} />
+      </Routes>
     </>
-  
   );
 }
 
